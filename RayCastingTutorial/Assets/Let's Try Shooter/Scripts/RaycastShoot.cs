4b6be1ac2601dd6bf33517dc54ad597709cee5e3
cbyu_VCL-09 <cbyu@scs.ryerson.ca>
2017-01-30 15:52:27 -0500

    Adding Assets let's try game dev

---
+++ RayCastingTutorial/Assets/Let's Try Shooter/Scripts/RaycastShoot.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaycastShoot : MonoBehaviour {
+
+
+    public int gunDamage = 1;
+    public float fireRate = 0.25f;
+    public float weaponRange = 50f;//50 units of range
+    public float hitForce = 100f;
+    public Transform gunEnd;
+
+    private Camera fpsCam;
+    private WaitForSeconds shotDuration = new WaitForSeconds(0.7f);//determine how long to keep in game view
+    private AudioSource gunAudio;
+    private LineRenderer laserLine;
+    private float nextFire;//Time player will be allowed to fire
+
+	// Use this for initialization
+	void Start () {
+        laserLine = GetComponent<LineRenderer>();
+        gunAudio = GetComponent<AudioSource>();
+        fpsCam = GetComponentInParent<Camera>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetButtonDown("Fire1") && Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+            StartCoroutine(ShotEffect());
+            Vector3 rayOrigin = fpsCam.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 0));
+        }
+	}
+
+    private IEnumerator ShotEffect()
+    {
+        gunAudio.Play();
+        laserLine.enabled = true;
+        yield return shotDuration;
+        laserLine.enabled = false;
+
+    }
+}
