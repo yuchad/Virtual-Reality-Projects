68d77487b705ee2147275bab3e6ba3f2d71187ad
cbyu_VCL-08 <cbyu@scs.ryerson.ca>
2017-01-31 16:44:39 -0500

    Blowing up balloons and stuff

--- Lab2/Assets/Scripts/ControllerInput.cs
+++ Lab2/Assets/Scripts/ControllerInput.cs
@@ -9,8 +9,9 @@ public class ControllerInput : MonoBehaviour {
     public Rigidbody blueBalloon;
     public Rigidbody greenBalloon;
     public Rigidbody yellowBalloon;
-   
 
+    private bool blowUpFinished;
+    private Rigidbody balloonInstance;
     private SteamVR_TrackedObject trackedObj;
     private SteamVR_Controller.Device contDevice;
     // Use this for initialization
@@ -30,43 +31,88 @@ public class ControllerInput : MonoBehaviour {
         if (contDevice.GetPressDown(SteamVR_Controller.ButtonMask.Touchpad))
         {
             Vector2 touchpad = (contDevice.GetAxis(Valve.VR.EVRButtonId.k_EButton_SteamVR_Touchpad));
-            print(touchpad);
 
             //control up on trackpad
             if(touchpad.y > 0.65f)
             {
-                Rigidbody redBalloonInstance = Instantiate(redBalloon);
-                redBalloonInstance.transform.parent = this.transform;
-                redBalloonInstance.transform.position = this.transform.position;
-
+                makeBalloon(redBalloon);
+                StartCoroutine(ScaleOverTime(1));
+                blowUpFinished = true;
             }
 
             //control down on trackpad
             else if (touchpad.y < -0.65f)
             {
-                Rigidbody blueBalloonInstance = Instantiate(blueBalloon);
-                blueBalloonInstance.transform.parent = this.transform;
-                blueBalloonInstance.transform.position = this.transform.position;
+                makeBalloon(blueBalloon);
+                StartCoroutine(ScaleOverTime(1));
+                blowUpFinished = true;
             }
 
             //Control left on the touchpad
             else if (touchpad.x < 0.65f)
             {
-                Rigidbody greenBalloonInstance = Instantiate(greenBalloon);
-                greenBalloonInstance.transform.parent = this.transform;
-                greenBalloonInstance.transform.position = this.transform.position;
+                makeBalloon(greenBalloon);
+                StartCoroutine(ScaleOverTime(1));
+                blowUpFinished = true;
             }
 
             //Control right on the touchpad
             else if (touchpad.x > -0.65f)
             {
-                Rigidbody yellowBalloonInstance = Instantiate(yellowBalloon);
-                yellowBalloonInstance.transform.parent = this.transform;
-                yellowBalloonInstance.transform.position = this.transform.position;
+                makeBalloon(yellowBalloon);
+                StartCoroutine(ScaleOverTime(1));
+                blowUpFinished = true;
             }
 
         }
 
+        if (contDevice.GetPressUp(SteamVR_Controller.ButtonMask.Touchpad))
+        {
+            if(blowUpFinished == true)
+            {
+                releaseBaloon();
+                blowUpFinished = false;
+            }
+            
+        }
+
+
+
+    }
+
+    void makeBalloon(Rigidbody balloon)
+    {
+        balloonInstance = Instantiate(balloon);
+        balloonInstance.transform.parent = this.transform;
+        balloonInstance.transform.position = this.transform.position + this.transform.forward * 0.12f;
         
     }
+
+    void releaseBaloon()
+    {
+        if (balloonInstance != null)
+        {
+            balloonInstance.transform.parent = null;
+            balloonInstance = null;
+        }
+    }
+
+    IEnumerator ScaleOverTime(float time)
+    {
+        Vector3 originalScale = balloonInstance.transform.localScale;
+        Vector3 endScale = new Vector3(0.4f, 0.4f, 0.4f);
+        float currentTime = 0.0f;
+
+        do
+        {
+            balloonInstance.transform.localScale = Vector3.Lerp(originalScale, endScale, currentTime / time);
+            currentTime += Time.deltaTime;
+            yield return null;
+        } while (currentTime <= time);
+       
+    
+        
+    }
+
+
 }
