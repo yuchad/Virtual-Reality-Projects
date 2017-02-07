9d5331774f41df6f4469ced9de113970139a5414
cbyu_VCL-09 <cbyu@scs.ryerson.ca>
2017-02-07 12:34:59 -0500

    added reticle

--- Lab3/Assets/Scripts/ControllerInput.cs
+++ Lab3/Assets/Scripts/ControllerInput.cs
@@ -4,14 +4,26 @@ using UnityEngine;
 
 public class ControllerInput : MonoBehaviour {
 
+    public Transform cameraRigTransform;
+    public GameObject teleporterPrefab;
+    public Transform headTransform;
+    public Vector3 teleporterOffset;
+    public LayerMask teleporterMask;
+
     private LineRenderer laserLine;
     private SteamVR_TrackedObject trackedObj;
     private SteamVR_Controller.Device contDevice;
+    private GameObject reticle;
+    private Transform teleportReticleTransform;
+    private bool canTeleport;
+    private Vector3 hitpoint;
     // Use this for initialization
     void Start () {
         trackedObj = this.GetComponent<SteamVR_TrackedObject>();
         contDevice = SteamVR_Controller.Input((int)trackedObj.index);
         laserLine = GetComponent<LineRenderer>();
+        reticle = Instantiate(teleporterPrefab);
+        teleportReticleTransform = reticle.transform;
     }
 	
 	// Update is called once per frame
@@ -19,17 +31,28 @@ public class ControllerInput : MonoBehaviour {
         if (contDevice.GetTouch(SteamVR_Controller.ButtonMask.Touchpad)) {
             laserLine.enabled = true;
             laserLine.SetPosition(0, this.transform.position);
+            laserLine.SetPosition(1, this.transform.position + this.transform.forward * 50);
             RaycastHit hit;
-            if (Physics.Raycast(this.transform.position, this.transform.forward, out hit, 50)) {
-                if(hit.collider.gameObject.tag == "floor") {
-                    print("hello");
+            if (Physics.Raycast(trackedObj.transform.position, transform.forward, out hit, 100)) { 
+                hitpoint = hit.point;
+                if (hit.collider.gameObject.tag == "floor") {
+                    reticle.SetActive(true);
+                    teleportReticleTransform.position = hitpoint + teleporterOffset;
+                    canTeleport = true;
+
                 }
+                
             }
-                laserLine.SetPosition(1, this.transform.position + this.transform.forward * 50);
+           
+               
+            
         }
         else if (contDevice.GetTouchUp(SteamVR_Controller.ButtonMask.Touchpad)) {
             laserLine.enabled = false;
+            reticle.SetActive(false);
         }
         
 	}
+
+   
 }
