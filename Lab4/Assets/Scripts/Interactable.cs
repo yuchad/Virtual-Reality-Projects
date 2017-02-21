6afd31b0e7ae1631674e3562bb0d9c83c7b59eeb
cbyu_VCL-01 <cbyu@scs.ryerson.ca>
2017-02-21 13:00:45 -0500

    adding some scene stuff

---
+++ Lab4/Assets/Scripts/Interactable.cs
@@ -0,0 +1,79 @@
+﻿//
+//  How to create your own Interactable:
+//  - Create a subclass of this class.
+//  - Override any of the virtual methods.
+//  - You can use attachedController.transform to get the controller's position
+//  - You can also use attachedController.input to query for input and send haptic pulses.
+//
+//  Your Interactable must have:
+//  - at least one collider on any of its child objects with its layer set to "Interactable"
+//  - A rigidbody on the same object as this script.
+//
+//  A user will begin interacting with this object if they place the controller on the its collider and press the trigger.
+//  The interaction will not end until they release the trigger. You can manually stop it by calling DetachController().
+//
+//  To make sure we only collide with the actual handles and knobs, We'll use the collisionMatrix.
+//  The controller's layers are set to "Controllers" and the Interactables' are set to "Interactables"
+//  We can then restrict collisions in Edit->ProjectSettings->Physics 
+//
+//  See WandController.cs to see how this object is detected.
+//
+
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public abstract class Interactable : MonoBehaviour
+{
+    // This allows the control to be grabbed by another wand while it is being held.
+    public bool Stealable = true;
+    // We restrict the attachedController property so that it can only be set using the TryAttach method.
+    public WandController attachedController { get; private set; }
+
+    //Returns true if the controller was attached correctly.
+    public bool TryAttach(WandController ctrl) {
+        //Only attach if the controller is not holding something already.
+        if (ctrl.heldItem != null) return false;
+        //Steal object from other controller.
+        if (attachedController != null)
+        {
+            if (!Stealable) return false;
+            DetachController();
+        }
+
+        attachedController = ctrl;
+        OnBeginInteraction();
+        // Perhaps DetachController was called from OnBeginInteraction.
+        if (attachedController == null) return false;
+        return true;
+    }
+
+
+    public void DetachController()
+    {
+        OnEndInteraction();
+        if (attachedController.heldItem == this) attachedController.OnItemDetach(this);
+        else Debug.LogError("Controller state (HeldItem) was incorrect. Tried to detach " + this + " while holding " +attachedController.heldItem);
+        attachedController = null;
+    }
+
+
+
+    //Called when the user begins interacting with this object. 
+    protected virtual void OnBeginInteraction() { }
+
+    //Called  when the user stops interacting with this object. 
+    protected virtual void OnEndInteraction() { }
+
+    //No need for OnInteractionStay. You can issue updates for your object in Update() and check if attachedController is null;
+
+    //Called by WandController when an unattached controller overlaps this object's collider.
+    public virtual void OnHoverEnter(WandController ctrl) { }
+
+    //Called by WandController when an unattached controller overlaps this object's collider.
+    public virtual void OnHoverExit(WandController ctrl) { }
+
+    //Called by WandController on each frame an unattached controller overlaps this object's collider.
+    public virtual void OnHoverStay(WandController ctrl) { }
+
+
+}
---
+++ Lab4/Assets/Scripts/WandController.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using Valve.VR;
+[RequireComponent(typeof(SteamVR_TrackedObject))]
+public class WandController : MonoBehaviour
+{
+    //Made Public so that it made be accessed from the Interactables
+    public SteamVR_Controller.Device input;
+    //Only one held item for now.
+    public Interactable heldItem;
+
+    private void Start()
+    {
+        
+        //Get Input obj
+        var trackedObj = this.GetComponent<SteamVR_TrackedObject>();
+        input = SteamVR_Controller.Input((int)trackedObj.index);
+
+
+        // To make sure we only collide with the actual handles and knobs, We'll use the collisionMatrix.
+        // The controller's layers are set to "Controllers" and the Interactables' are set to "Interactables"
+        // We can then restrict collisions in Edit->ProjectSettings->Physics 
+
+        if (gameObject.layer != LayerMask.NameToLayer("Controllers")) Debug.LogError("Controllers should be in 'Controllers' Collision Layer");
+    }
+
+    private void Update()
+    {
+        //Let go of item. We don't clear heldItem here, as it's take care of in OnItemDetach.
+        bool triggerReleased = input.GetPressUp(EVRButtonId.k_EButton_SteamVR_Trigger);
+        if(heldItem && triggerReleased)
+        {
+            heldItem.DetachController();
+        }
+    }
+
+    public void OnItemDetach(Interactable item)
+    {
+        heldItem = null;
+    }
+
+
+
+    //Send HoverEnter message to Interactable
+    private void OnTriggerEnter(Collider other)
+    {
+        // The collider we collided with may not be the root object of the interactable.
+        // We assume that the rigidbody is.
+        if (other.attachedRigidbody == null) return;
+        var interactable = other.attachedRigidbody.GetComponent<Interactable>();
+        if (interactable == null) return;
+        if (heldItem == interactable) return;
+        interactable.OnHoverEnter(this);
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.attachedRigidbody == null) return;
+        var interactable = other.attachedRigidbody.GetComponent<Interactable>();
+        if (interactable == null) return;
+        if (heldItem == interactable) return;
+
+        // Start Interacting 
+        if (input.GetPressDown(EVRButtonId.k_EButton_SteamVR_Trigger))
+        {
+            bool didAttach = interactable.TryAttach(this);
+            if (didAttach) heldItem = interactable;
+        }
+        else interactable.OnHoverStay(this);
+    }
+    //Send HoverExit message to Interactable
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.attachedRigidbody == null) return;
+        var interactable = other.attachedRigidbody.GetComponent<Interactable>();
+        if (interactable == null) return;
+        if (heldItem == interactable) return;
+        interactable.OnHoverStay(this);
+    }
+
+}
